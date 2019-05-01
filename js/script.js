28b5acee68c356ad8a5b6d2eb8339412cbe1d4b9
Artur Ballod <artur.ballod@gmail.com>
2019-05-01 02:41:27 +0200

    #event-full-to-768

--- js/script.js
+++ js/script.js
@@ -32,8 +32,8 @@ $(document).ready(function() {
     arrows: true,
     dots: true,
 
-    prevArrow: '<div class="prev-1"></div>',
-    nextArrow: '<div class="next-1"></div>',
+    prevArrow: '<div class="prev"></div>',
+    nextArrow: '<div class="next"></div>',
   });
 });
 
